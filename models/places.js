22ee6dbf0c5768a161d13c491e73bf6123179c56
Courtney Hatcher <courtneyehatcher@gmail.com>
2022-09-16 08:50:41 -0700

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    name: 'Sad Salads',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Salads & Healthy stuff',
+    pic: '/images/salad.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Las Vegas',
+    state: 'Nevada',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/code-cat.jpg'
+}]
+
