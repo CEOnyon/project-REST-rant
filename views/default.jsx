899d88cf22dabe6c9566cbb54f670fc27be57623
Courtney Hatcher <courtneyehatcher@gmail.com>
2022-09-17 11:15:00 -0700

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -9,6 +9,19 @@ function Def (html) {
                 <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">Rest-Rant</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                         </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
+                            <a class="nav-item nav-link" href="/places">Places</a>
+                            <a class="nav-item nav-link" href="/places/new">Add a place</a>
+                        </div>
+                    </div>
+                </nav>
                 {html.children}
             </body>
         </html>
