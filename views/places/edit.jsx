0f3cd60a65232c33741b772146676bce3389a90a
Courtney Hatcher <courtneyehatcher@gmail.com>
2022-09-18 13:19:48 -0700

    Added the edit functionality

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ function edit_form (data) {
         <Def>
           <main>
                 <h1>Edit Place</h1>
-                <form method='POST' action={`/places/${data.place.id}?_method=PUT`}>
+                <form method='POST' action={`/places/${data.id}?_method=PUT`}>
                     <div className='row'>
                         <div className='form-gorup col-sm-6'>
                             <label>Place Name</label>
@@ -14,7 +14,7 @@ function edit_form (data) {
                         </div>
                         <div className='form-gorup col-sm-6'>
                             <label htmlFor="pic">Place Picture</label>
-                            <input className='form-control' id="pic" name="pic" value={data.place.pic}/>
+                            <input className='form-control' type="url" id="pic" name="pic" value={data.place.pic}/>
                         </div>
                         <div className='form-gorup col-sm-6'>
                             <label htmlFor="city">City</label>
