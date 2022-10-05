7f2ba2c19a6f042605eefd46432746cd46ec9ca1
Courtney Hatcher <courtneyehatcher@gmail.com>
2022-10-05 13:14:49 -0700

    Print env vars.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
+console.log(process.env);
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
