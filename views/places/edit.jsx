286fd06c36c18452f9cd8e16a5752c3f63dda36a
Courtney Hatcher <courtneyehatcher@gmail.com>
2022-10-04 17:46:16 -0700

    populating data for star ratings

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ function edit_form (data) {
         <Def>
           <main>
                 <h1>Edit Place</h1>
-                <form method='POST' action={`/places/${data.id}?_method=PUT`}>
+                <form method='POST' action={`/places/${data.place.id}?_method=PUT`}>
                     <div className='row'>
                         <div className='form-gorup col-sm-6 col-md-4 col-lg-3'>
                             <label>Place Name</label>
@@ -28,8 +28,16 @@ function edit_form (data) {
                             <label htmlFor="pic">Cuisines</label>
                             <input className='form-control' id="cuisines" name="cuisines" value={data.place.cuisines} required/>
                         </div>
+                        <div className="form-group col-sm-4">
+                            <label htmlFor="founded">Founded</label>
+                            <input className="form-control" 
+                            id="founded" 
+                            name="founded" 
+                            value={data.place.founded} 
+                            />
+                        </div>
                     </div>
-                    <input className='btn btn-primary' type="submit" value="Add Place"/>
+                    <input className='btn btn-primary' type="submit" value="Update Place"/>
                 </form>
             </main>
         </Def>
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -15,7 +15,7 @@ function new_form(data) {
             <main>
                 <h1>Add a new place</h1>
                 {message}
-                <form method='POST' action='/places'>
+                <form method='POST' action='/places/'>
                     <div className='form-gorup'>
                         <label>Place Name</label>
                         <input className='form-control' id='name' name='name'required/>
@@ -38,12 +38,11 @@ function new_form(data) {
                     </div>
                     <div className="form-group">
                         <label htmlFor="founded">Founded Year</label>
-                        <input 
-                        type="number" 
-                        className="form-control" 
+                        <input className="form-control"
+                        type="number"  
                         id="founded" 
                         name="founded" 
-                        value={new Date().getFullYear()} />
+                        value={data.place.founded} />
                     </div>
                     <input className='btn btn-primary' type="submit" value="Add Place"/>
                 </form>
